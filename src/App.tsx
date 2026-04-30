a6780d5875235d56ace0974ce3e628491cb3673e
atheeth707 <atheeth707@gmail.com>
2026-04-30 21:04:41 +0530

    mmmmmm

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';[cite: 2, 3]
-import Layout from './components/Layout';[cite: 2, 3]
-import Home from './pages/Home';[cite: 2, 3]
-import Category from './pages/Category';[cite: 2, 3]
-import ToolPage from './pages/ToolPage';[cite: 2, 3]
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Category from './pages/Category';
+import ToolPage from './pages/ToolPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />[cite: 2, 3]
-          <Route path="category/:categoryId" element={<Category />} />[cite: 2, 3]
-          <Route path="tool/:toolId" element={<ToolPage />} />[cite: 2, 3]
+          <Route index element={<Home />} />
+          <Route path="category/:categoryId" element={<Category />} />
+          <Route path="tool/:toolId" element={<ToolPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App;[cite: 2, 3]
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react';[cite: 4]
-import { createRoot } from 'react-dom/client';[cite: 4]
-import App from './App.tsx';[cite: 4]
-import './index.css';[cite: 4]
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
-);[cite: 4]
+);
